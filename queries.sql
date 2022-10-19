79dabc7a7168577405cdd3859d9ff3f422d5a3de
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-10-20 01:24:57 +0530

    More queries

--- queries.sql
+++ queries.sql
@@ -24,11 +24,11 @@ SELECT * from animals;
 ROLLBACK;
 
 START Transaction;
-SAVEPOINT pt1;
 DELETE FROM animals * WHERE date_of_birth > '2022-01-01';
+SAVEPOINT pt1;
+UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO SAVEPOINT pt1;
-UPDATE animals SET weight_kg = weight_kg * -1;
-UPDATE animals SET weight_kg = weight_kg * -1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 
 SELECT count(*) AS number_of_animals FROM animals;
